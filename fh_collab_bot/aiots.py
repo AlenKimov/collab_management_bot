7d1950e33ad3d72288d64c3914e3288f62d25081
Alen <alen.kimov@gmail.com>
2023-03-02 16:29:55 +0300

    feat(aiots): asyncio_throttle для ограничения количества запросов к TweetScout API
    - Также теперь возвращает None, если не удалось узнать TSS проекта

--- fh_collab_bot/aiots.py
+++ fh_collab_bot/aiots.py
@@ -1,18 +1,34 @@
 from typing import Iterable
-import aiohttp
 import asyncio
 
+import aiohttp
+from aiohttp.client_exceptions import ContentTypeError
+from asyncio_throttle import Throttler
 
-async def _get_tweetscout_data(session: aiohttp.ClientSession, twitter_handle: str):
+from logger import logger
+
+throttler = Throttler(rate_limit=1, period=2)
+
+
+async def _get_tweetscout_data(session: aiohttp.ClientSession, twitter_handle: str) -> dict:
     url = 'https://tweetscout.io/api/v1/accounts/search'
     querystring = {'q': twitter_handle}
-    async with session.get(url, params=querystring) as response:
-        return await response.json()
+    async with throttler:
+        async with session.get(url, params=querystring) as response:
+            try:
+                return await response.json()
+            except ContentTypeError as exception:
+                logger.exception(exception)
+                logger.error(await response.text())
+                return {}
 
 
-async def get_tss(session: aiohttp.ClientSession, twitter_handle: str) -> int:
+async def get_tss(session: aiohttp.ClientSession, twitter_handle: str) -> int | None:
     data = await _get_tweetscout_data(session, twitter_handle)
-    tss = data['score']['value']
+    if 'score' not in data:
+        tss = None
+    else:
+        tss = data['score']['value']
     return tss
 
 
