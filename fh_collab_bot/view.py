975d4b87e134c4a4c570641f9cace01cc1a5bc0b
Alen <alen.kimov@gmail.com>
2023-03-06 20:34:18 +0300

    feat(view): небольшие стилистические изменения в выводе полной информации о проекте

--- fh_collab_bot/view.py
+++ fh_collab_bot/view.py
@@ -2,9 +2,9 @@ def project_data_representation(project_data: dict):
     project_info_message_parts = []
     if project_data['manager_telegram_id'] is not None:
         project_info_message_parts.append(f'Ведущий: {project_data["manager_telegram_id"]}\n')
-    project_info_message_parts.append(f'[{project_data["likes"]}/{project_data["dislikes"]}]')
+    project_info_message_parts.append(f'[+{project_data["likes"]}/-{project_data["dislikes"]}]')
     if project_data['tss'] is not None:
-        project_info_message_parts.append(f' ({project_data["tss"]})')
+        project_info_message_parts.append(f' ({project_data["tss"]:04d})')
     project_info_message_parts.append(f' <a href="https://twitter.com/{project_data["twitter_handle"]}">{project_data["twitter_handle"]}</a>')
     if project_data['discord_url'] is not None or project_data['discord_admin_nickname'] is not None:
         project_info_message_parts.append('\n')
