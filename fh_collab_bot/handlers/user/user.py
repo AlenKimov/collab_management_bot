5303b243be3a0997987b9c75e07daaf8ff0a560b
Alen <alen.kimov@gmail.com>
2023-03-14 10:10:44 +0300

    fix: теперь сессия в базе данных открывается с помощью контекстного менеджера
    Во избежание ошибки https://sqlalche.me/e/20/3o7r

--- fh_collab_bot/handlers/user/user.py
+++ fh_collab_bot/handlers/user/user.py
@@ -20,60 +20,60 @@ from keyboards.inline.callback_data import request_tss_cb
 @dp.message_handler(commands=['my', 'me'])
 async def cmd_my_projects(message: Message):
     """Выводит список всех взятыых менеджером проектов, отсортированных по дате взятия"""
-    db_session = DatabaseSession()
-    manager: Manager = db_session.query(Manager).filter_by(telegram_id=message.from_user.id).scalar()
-    projects: list[Project] = manager.projects
-    for project in projects:
-        logger.debug(f'Командой {message.text}'
-                     f' менеджер {message.from_user.id} (@{message.from_user.username})'
-                     f' запросил {project}')
-        project_management_keyboard = await create_project_management_inline_keyboard(message.from_user.id, project)
-        await message.answer(project.get_full_info(), disable_web_page_preview=True,
-                             reply_markup=project_management_keyboard)
+    with DatabaseSession() as db_session:
+        manager: Manager = db_session.query(Manager).filter_by(telegram_id=message.from_user.id).scalar()
+        projects: list[Project] = manager.projects
+        for project in projects:
+            logger.debug(f'Командой {message.text}'
+                         f' менеджер {message.from_user.id} (@{message.from_user.username})'
+                         f' запросил {project}')
+            project_management_keyboard = await create_project_management_inline_keyboard(message.from_user.id, project)
+            await message.answer(project.get_full_info(), disable_web_page_preview=True,
+                                 reply_markup=project_management_keyboard)
 
 
 @dp.message_handler(commands=['best'])
 async def cmd_best_projects(message: Message):
     """Выводит список из 10-ти никем не взятых проектов в порядке убывания лайков и в порядке убывания TSS"""
-    db_session = DatabaseSession()
-    projects: list[Project] = (
-        db_session.query(Project)
-        .filter_by(manager_telegram_id=None)
-        .order_by(Project.likes.desc())
-        .order_by(Project.tss.desc())
-        .limit(10)
-        .all()
-    )
-    for project in projects:
-        logger.debug(f'Командой {message.text}'
-                     f' менеджер {message.from_user.id} (@{message.from_user.username})'
-                     f' запросил {project}')
-        project_management_keyboard = await create_project_management_inline_keyboard(message.from_user.id, project)
-        await message.answer(project.get_full_info(), disable_web_page_preview=True,
-                             reply_markup=project_management_keyboard)
+    with DatabaseSession() as db_session:
+        projects: list[Project] = (
+            db_session.query(Project)
+            .filter_by(manager_telegram_id=None)
+            .order_by(Project.likes.desc())
+            .order_by(Project.tss.desc())
+            .limit(10)
+            .all()
+        )
+        for project in projects:
+            logger.debug(f'Командой {message.text}'
+                         f' менеджер {message.from_user.id} (@{message.from_user.username})'
+                         f' запросил {project}')
+            project_management_keyboard = await create_project_management_inline_keyboard(message.from_user.id, project)
+            await message.answer(project.get_full_info(), disable_web_page_preview=True,
+                                 reply_markup=project_management_keyboard)
 
 
 @dp.message_handler(commands=['new'])
 async def cmd_best_projects(message: Message):
     """Выводит список из 10-ти никем не взятых проектов без оценок,
     отсортированных по новизне и в порядке убывания TSS"""
-    db_session = DatabaseSession()
-    projects: list[Project] = (
-        db_session.query(Project)
-        .filter_by(manager_telegram_id=None)
-        .filter_by(likes=0, dislikes=0)
-        .order_by(Project.created_at.desc())
-        .order_by(Project.tss.desc())
-        .limit(10)
-        .all()
-    )
-    for project in projects:
-        logger.debug(f'Командой {message.text}'
-                     f' менеджер {message.from_user.id} (@{message.from_user.username})'
-                     f' запросил {project}')
-        project_management_keyboard = await create_project_management_inline_keyboard(message.from_user.id, project)
-        await message.answer(project.get_full_info(), disable_web_page_preview=True,
-                             reply_markup=project_management_keyboard)
+    with DatabaseSession() as db_session:
+        projects: list[Project] = (
+            db_session.query(Project)
+            .filter_by(manager_telegram_id=None)
+            .filter_by(likes=0, dislikes=0)
+            .order_by(Project.created_at.desc())
+            .order_by(Project.tss.desc())
+            .limit(10)
+            .all()
+        )
+        for project in projects:
+            logger.debug(f'Командой {message.text}'
+                         f' менеджер {message.from_user.id} (@{message.from_user.username})'
+                         f' запросил {project}')
+            project_management_keyboard = await create_project_management_inline_keyboard(message.from_user.id, project)
+            await message.answer(project.get_full_info(), disable_web_page_preview=True,
+                                 reply_markup=project_management_keyboard)
 
 
 @dp.callback_query_handler(hide_keyboard_cb.filter())
@@ -92,25 +92,25 @@ async def vote_cb_handler(query: CallbackQuery, callback_data: dict):
     await query.answer(cache_time=1)
     project_twitter_handle = callback_data['project_twitter_handle']
     vote_type = int(callback_data['vote_type'])
-    db_session = DatabaseSession()
-    db_session.merge(Vote(manager_telegram_id=query.from_user.id, project_twitter_handle=project_twitter_handle,
-                          vote_type=vote_type))
-    db_session.commit()
-    project: Project = db_session.query(Project).filter_by(twitter_handle=project_twitter_handle).scalar()
-    await update_project_management_keyboard(query, project)
+    with DatabaseSession() as db_session:
+        db_session.merge(Vote(manager_telegram_id=query.from_user.id, project_twitter_handle=project_twitter_handle,
+                              vote_type=vote_type))
+        db_session.commit()
+        project: Project = db_session.query(Project).filter_by(twitter_handle=project_twitter_handle).scalar()
+        await update_project_management_keyboard(query, project)
 
 
 @dp.callback_query_handler(delete_vote_cb.filter())
 async def delete_vote_cb_handler(query: CallbackQuery, callback_data: dict):
     await query.answer(cache_time=1)
     project_twitter_handle = callback_data['project_twitter_handle']
-    db_session = DatabaseSession()
-    vote: Vote = db_session.query(Vote).filter_by(manager_telegram_id=query.from_user.id,
-                                                  project_twitter_handle=project_twitter_handle).scalar()
-    db_session.delete(vote)
-    db_session.commit()
-    project: Project = db_session.query(Project).filter_by(twitter_handle=project_twitter_handle).scalar()
-    await update_project_management_keyboard(query, project)
+    with DatabaseSession() as db_session:
+        vote: Vote = db_session.query(Vote).filter_by(manager_telegram_id=query.from_user.id,
+                                                      project_twitter_handle=project_twitter_handle).scalar()
+        db_session.delete(vote)
+        db_session.commit()
+        project: Project = db_session.query(Project).filter_by(twitter_handle=project_twitter_handle).scalar()
+        await update_project_management_keyboard(query, project)
 
 
 @dp.callback_query_handler(lead_project_cb.filter())
@@ -118,48 +118,49 @@ async def lead_cb_handler(query: CallbackQuery, callback_data: dict):
     await query.answer(cache_time=1)
     project_twitter_handle = callback_data['project_twitter_handle']
     want_to_lead = int(callback_data['want_to_lead'])
-    db_session = DatabaseSession()
-    project: Project = db_session.query(Project).filter_by(twitter_handle=project_twitter_handle).scalar()
-    if want_to_lead == 1:
-        project.manager_telegram_id = query.from_user.id
-        db_session.commit()
-    elif want_to_lead == 0:
-        project.manager_telegram_id = None
-        db_session.commit()
-    await update_project_management_keyboard(query, project)
+    with DatabaseSession() as db_session:
+        project: Project = db_session.query(Project).filter_by(twitter_handle=project_twitter_handle).scalar()
+        if want_to_lead == 1:
+            project.manager_telegram_id = query.from_user.id
+            db_session.commit()
+        elif want_to_lead == 0:
+            project.manager_telegram_id = None
+            db_session.commit()
+        await update_project_management_keyboard(query, project)
 
 
 @dp.callback_query_handler(request_tss_cb.filter())
 async def request_tss_cb_handler(query: CallbackQuery, callback_data: dict):
     await query.answer(cache_time=60)
     project_twitter_handle = callback_data['project_twitter_handle']
-    db_session = DatabaseSession()
-    project: Project = db_session.query(Project).filter_by(twitter_handle=project_twitter_handle).scalar()
-    async with aiohttp.ClientSession() as session:
-        new_tss = await get_tss(session, project_twitter_handle)
-    if project.tss != new_tss:
-        project.tss = new_tss
-        db_session.commit()
-        await update_project_management_keyboard(query, project)
+    with DatabaseSession() as db_session:
+        project: Project = db_session.query(Project).filter_by(twitter_handle=project_twitter_handle).scalar()
+        async with aiohttp.ClientSession() as session:
+            new_tss = await get_tss(session, project_twitter_handle)
+        if project.tss != new_tss:
+            project.tss = new_tss
+            db_session.commit()
+            await update_project_management_keyboard(query, project)
 
 
 @dp.message_handler(content_types='text')
 async def cmd_check_twitter(message: Message):
     twitter_handles = to_twitter_handles(message.text.split('\n'))
-    db_session = DatabaseSession()
-    async with aiohttp.ClientSession() as session:
-        for twitter_handle in twitter_handles:
-            is_in_table = db_session.query(
-                db_session.query(Project).filter_by(twitter_handle=twitter_handle).exists()
-            ).scalar()
-            if not is_in_table:
-                tss = await get_tss(session, twitter_handle)
-                project = Project(twitter_handle=twitter_handle, tss=tss)
-                db_session.add(project)
-                db_session.commit()
-            else:
-                project: Project = db_session.query(Project).filter_by(twitter_handle=twitter_handle).scalar()
-            logger.debug(project)
-            project_management_keyboard = await create_project_management_inline_keyboard(message.from_user.id, project)
-            await message.answer(project.get_full_info(), disable_web_page_preview=True,
-                                 reply_markup=project_management_keyboard)
+    logger.debug(f'Менеджер {message.from_user.id} (@{message.from_user.username})'
+                 f' внес следующие проекты: {twitter_handles}')
+    with DatabaseSession() as db_session:
+        async with aiohttp.ClientSession() as session:
+            for twitter_handle in twitter_handles:
+                is_in_table = db_session.query(
+                    db_session.query(Project).filter_by(twitter_handle=twitter_handle).exists()
+                ).scalar()
+                if not is_in_table:
+                    tss = await get_tss(session, twitter_handle)
+                    project = Project(twitter_handle=twitter_handle, tss=tss)
+                    db_session.add(project)
+                    db_session.commit()
+                else:
+                    project: Project = db_session.query(Project).filter_by(twitter_handle=twitter_handle).scalar()
+                project_management_keyboard = await create_project_management_inline_keyboard(message.from_user.id, project)
+                await message.answer(project.get_full_info(), disable_web_page_preview=True,
+                                     reply_markup=project_management_keyboard)
