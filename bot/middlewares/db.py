3827d491a18209ddc65bcdf0abf25e79f8e8e305
Alen <alen.kimov@gmail.com>
2023-03-23 16:26:25 +0300

    refactor: немного синтаксического сахара

--- bot/middlewares/db.py
+++ bot/middlewares/db.py
@@ -1,4 +1,4 @@
-from typing import Callable, Awaitable, Dict, Any
+from typing import Callable, Awaitable, Any
 
 from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject
@@ -7,15 +7,14 @@ from sqlalchemy.ext.asyncio import async_sessionmaker
 
 class DbSessionMiddleware(BaseMiddleware):
     def __init__(self, session_pool: async_sessionmaker):
-        super().__init__()
         self.session_pool = session_pool
 
     async def __call__(
-            self,
-            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-            event: TelegramObject,
-            data: Dict[str, Any],
+        self,
+        handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: dict[str, Any],
     ) -> Any:
         async with self.session_pool() as session:
-            data["session"] = session
+            data['session'] = session
             return await handler(event, data)
